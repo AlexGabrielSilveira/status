607122be742aef107d6707568b4b1eeaaf219711
AlexGabrielSilveira <alexgabriel1217@gmail.com>
2023-06-04 15:46:58 -0300

    refazer a home com uma nova API, e fazer o grafico funcional

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,12 @@
 "use client"
 
-import { useEffect, useState } from 'react'
 import styles from './page.module.css'
-import Infos from '@/components/infos/Infos'
 
 export default function Home() { 
-  const[load, setLoad] = useState(true)
-  const[stocksNames, setStocksNames] = useState([])
-
-  
-  function getStocksNames() {
-    fetch('https://brapi.dev/api/available')
-      .then(res => res.json())
-      .then(res => {
-        setStocksNames(res.stocks.slice(0,50))
-        setLoad(false)
-      })
-  }
-  
-
-  useEffect(() => {
-    getStocksNames()
-  }, [])
   
   return (
     <>
-      {load == false ? (
-        <div className={styles.infos}>
-          <h2>Sto<span>cks</span></h2>
-          <div>
-            <Infos key={stocksNames.symbol} stocks={stocksNames} type="stocks"/>
-          </div>
-        </div>
-      ) : <h1>carregando</h1>}
+      <h1 className={styles.title}>A página home está em <span>desenvolvimento,</span> mas fique avontade para pesuisar sobre <span>qualquer ação ( B3 ) </span>que quiser!</h1>
     </>
   )
 }
