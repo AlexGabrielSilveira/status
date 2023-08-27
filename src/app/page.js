1d5bfb4d02cbcbfd8dbe06dc60d41613cc015c87
AlexGabrielSilveira <alexgabrielyou@gmail.com>
2023-08-27 17:09:51 -0300

    pagina home melhorada

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,114 @@
 "use client"
-import { useEffect, useState } from 'react'
+import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
 
 export default function Home() {  
 
-  let banks = [ "BBAS3","SANB11","BBDC3","ITUB4","ITSA4","CXSE3","BMGB4","BPAN4","BIDI11","ABCB4","B3SA3" ]
-  let eletrics = [ "CPLE6","ELET6","TAEE11","TRPL4","ENGI11","EGIE3","AURE3","AESB3" ]
+  let banks = [
+    {
+      ticker: "BBAS3",
+      img: "/BBAS3.jfif"
+    },
+    {
+      ticker: "SANB11",
+      img: "/SANB11.jfif"
+    }, 
+    {
+      ticker: "BBDC3",
+      img: "/BBDC3.jfif"
+    },
+    {
+      ticker: "ITUB4",
+      img: "/ITUB4.jfif"
+    },
+    {
+      ticker: "ITSA4",
+      img: "/ITSA4.jfif"
+    },
+    {
+      ticker: "CXSE3",
+      img: "/CXSE3.png"
+    },
+    {
+      ticker: "BMGB4",
+      img: "/BMGB4.png"
+    }, 
+    {
+      ticker: "BPAN4",
+      img: "/BPAN4.jfif"
+    },
+    {
+      ticker: "BIDI11",
+      img: "/BIDI11.png"
+    },
+    {
+      ticker: "ABCB4",
+      img: "/ABCB4.png"
+    },
+    {
+      ticker: "B3SA3",
+      img: "/B3SA3.jfif"
+    }
+  ]    
+    
+  let eletrics = [ 
+    {
+      ticker: "CPLE6",
+      img: "/CPLE6.png"
+    },
+    {
+      ticker: "ELET6",
+      img:"/ELET6.jfif"
+    },
+    {
+      ticker: "TAEE11",
+      img: "/TAEE11.jfif"
+    },
+    {
+      ticker: "TRPL4",
+      img: "/TRPL4.jfif"
+    }, 
+    {
+      ticker: "ENGI11",
+      img: "/ENGI11.jfif"
+    }, 
+    {
+      ticker: "EGIE3",
+      img: "/EGIE3.jfif"
+    },
+    {
+      ticker: "AURE3",
+      img: "/AURE3.png"
+    }, 
+    {
+      ticker: "AESB3",
+      img: "/AESB3.jpg"
+    }
+  ]
 
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <h1>Bancos</h1>
-        <div>
+        <h1>BANCOS</h1>
+        <div className={styles.card_summary}>
           {banks.map(bank => (
             // eslint-disable-next-line react/jsx-key
-            <Link href={`/stocks/${bank}`}>{bank}</Link>
+              <Link href={`/stocks/${bank.ticker}`} key={bank.ticker}>{bank.ticker}
+                <Image src={bank.img} width={1280} height={720} alt={bank.ticker} />
+              </Link>
           ))}
         </div>
       </div>
       <div className={styles.card}>
-      <h1>Eletricas</h1>
-        <div>
-          {eletrics.map(eletric => (
-              // eslint-disable-next-line react/jsx-key
-              <Link href={`/stocks/${eletric}`}>{eletric}</Link>
-            ))}
+        <h1>Eletricas</h1>
+        <div className={styles.card_summary}> 
+        {eletrics.map(eletric => (
+            // eslint-disable-next-line react/jsx-key
+              <Link href={`/stocks/${eletric.ticker}`} key={eletric.ticker}>{eletric.ticker}
+                <Image src={eletric.img} width={1280} height={720} alt={eletric.ticker} />
+              </Link>
+          ))}
         </div>
       </div>
     </div>  
